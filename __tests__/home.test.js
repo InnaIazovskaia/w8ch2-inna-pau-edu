5c1baac4be1a6ddc1a5a4a65da4ed149801ea66d
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-03-03 23:38:55 +0100

    add render tuits text on homepage test

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -1,4 +1,6 @@
-import { getServerSideProps } from "../pages";
+import { screen, render } from "@testing-library/react";
+import Home, { getServerSideProps } from "../pages";
+import "whatwg-fetch";
 
 describe("Given a getServerSideProps function", () => {
   describe("When the home page it's instantiated", () => {
@@ -18,13 +20,40 @@ describe("Given a getServerSideProps function", () => {
 
       const response = {
         props: {
-          tutis: tuits,
+          tuits,
         },
       };
 
       const props = await getServerSideProps();
 
       expect(props).toEqual(response);
+    });
+  });
+});
+
+describe("Given a home page", () => {
+  describe("When it's rendered with a list of tuits", () => {
+    test("Then it should render the tutis", async () => {
+      const tuits = [
+        {
+          date: "2022-03-02T20:02:28.763+00:00",
+          likes: 7,
+          text: "first tuit",
+        },
+        {
+          date: "2022-04-02T20:02:28.763+00:00",
+          likes: 72,
+          text: "second tuit",
+        },
+      ];
+
+      render(<Home tuits={tuits} />);
+
+      const findFirstText = screen.getByText("first tuit");
+      const findSecondText = screen.getByText(tuits[1].text);
+
+      expect(findFirstText).toBeInTheDocument();
+      expect(findSecondText).toBeInTheDocument();
     });
   });
 });
